2e19fbe363b6eaaf3cc80a231e39d8e4eba84200
Kellar Elliott <kellarelliott@gmail.com>
2019-10-24 00:21:11 +0000

    Payment

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,18 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :enrolled_in_current_course, only: [:show]
   def show
+
   end
 
   private
+
+  def enrolled_in_current_course
+    if current_user.enrolled_in?(current_lesson.section.course) != true
+      flash[:alert] = "You need to be enrolled in this course to see lesson details."
+      redirect_to course_path(@current_lesson.section.course)
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
